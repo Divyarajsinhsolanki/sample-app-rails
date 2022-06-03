b27701959cee086e86a31665271018bf66ae603b
Divyarajsinhsolanki <solanki.divyarajsinhp@gmail.com>
2022-06-03 16:46:29 +0530

    smtp6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,23 +1,32 @@
 Rails.application.configure do
 
 
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.perform_caching = false
-  host = 'https://serene-spire-89363.herokuapp.com/' 
-  config.action_mailer.default_url_options = { :host => 'https://serene-spire-89363.herokuapp.com/', protocol: 'https' }
-  # config.require_master_key = true
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.perform_caching = false
+  # host = 'https://serene-spire-89363.herokuapp.com/' 
+  # config.action_mailer.default_url_options = { :host => 'https://serene-spire-89363.herokuapp.com/', protocol: 'https' }
+  # # config.require_master_key = true
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  address: 'smtp.gmail.com',
-  port: 587,
-  user_name: 'divyaraj.odoo.expert',
-  password: 'adhfprnijafrdbpv',
-  authentication: 'plain',
-  enable_starttls_auto: true
-  }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  # address: 'smtp.gmail.com',
+  # port: 587,
+  # user_name: 'divyaraj.odoo.expert',
+  # password: 'adhfprnijafrdbpv',
+  # authentication: 'plain',
+  # enable_starttls_auto: true
+  # }
 
+  # ActionMailer::Base.smtp_settings = {
+  # :address=> 'smtp.sendgrid.net',
+  # :port=> '587',
+  # :authentication => :plain,
+  # :user_name=> ENV['SENDGRID_USERNAME'],
+  # :password=> ENV['SENDGRID_PASSWORD'],
+  # :domain=> 'heroku.com',
+  # :enable_starttls_auto => true
+  # }
 
 
 
