6a447d7d1bb6433d218fb3b21462d92e883a6853
Divyarajsinhsolanki <solanki.divyarajsinhp@gmail.com>
2022-06-09 13:14:47 +0530

    follower

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
-  validates :name, :description, presence: true
+  validates :user_id, presence: true
+  validates :name, presence: true
+  validates :description, presence: true
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-has_many :items
+has_many :items, dependent: :destroy
 has_many :microposts, dependent: :destroy
 has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
 has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
@@ -8,6 +8,7 @@ has_many :following, through: :active_relationships, source: :followed
 has_many :followers, through: :passive_relationships, source: :follower
 
 attr_accessor :remember_token, :activation_token, :reset_token
+
 before_save :downcase_email
 before_create :create_activation_digest
 
@@ -94,7 +95,7 @@ before_create :create_activation_digest
 			self.email = email.downcase
 		end
 		
-			# Creates and assigns the activation token and digest.
+		# Creates and assigns the activation token and digest.
 		def create_activation_digest
 			self.activation_token = User.new_token
 			self.activation_digest = User.digest(activation_token)
